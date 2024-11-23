7c50ce225ef1b97890ecb3e844a09612a3987054
reachmohdaltaf <reachmohdaltaf@gmail.com>
2024-11-23 06:53:06 +0530

    newww

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@
 const Button = ({value}) => {
   return (
     <div>
-      <button className="bg-[#537EF8] hover:bg-[#3b62ce] p-2 rounded-lg text-gray-100">{value}</button>
+      <button className="bg-[#537EF8] active:scale-95 hover:bg-[#3b62ce] p-2 rounded-lg text-gray-100">{value}</button>
     </div>
   )
 }
